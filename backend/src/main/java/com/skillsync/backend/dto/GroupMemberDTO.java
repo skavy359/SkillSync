8e308f9a8ca9c83fb2aed49b50236658210f49ca
Kavy Sharma <skavy359@gmail.com>
2026-03-07 22:13:08 +0530

    backend: update GroupMemberDTO

---
+++ backend/src/main/java/com/skillsync/backend/dto/GroupMemberDTO.java
@@ -0,0 +1,21 @@
+package com.skillsync.backend.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupMemberDTO {
+    private Long userId;
+    private String userName;
+    private String userEmail;
+    private String userAbout;
+    private String role;
+    private LocalDateTime joinedAt;
+}
