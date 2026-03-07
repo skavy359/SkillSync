61de227eca7025779a3e494af65c12bf6724d092
Kavy Sharma <skavy359@gmail.com>
2026-03-07 22:13:08 +0530

    backend: update ForumPostUpvoteRepository

---
+++ backend/src/main/java/com/skillsync/backend/repository/ForumPostUpvoteRepository.java
@@ -0,0 +1,14 @@
+package com.skillsync.backend.repository;
+
+import com.skillsync.backend.model.ForumPost;
+import com.skillsync.backend.model.ForumPostUpvote;
+import com.skillsync.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface ForumPostUpvoteRepository extends JpaRepository<ForumPostUpvote, Long> {
+    Optional<ForumPostUpvote> findByUserAndPost(User user, ForumPost post);
+    boolean existsByUserAndPost(User user, ForumPost post);
+    List<ForumPostUpvote> findByUserAndPostIn(User user, List<ForumPost> posts);
+}
