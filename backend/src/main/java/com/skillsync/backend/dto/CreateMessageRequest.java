b19fc40b96c96771744530a67b4bdd7fa11a599f
Kavy Sharma <skavy359@gmail.com>
2026-03-07 22:13:08 +0530

    backend: update CreateMessageRequest

---
+++ backend/src/main/java/com/skillsync/backend/dto/CreateMessageRequest.java
@@ -0,0 +1,12 @@
+package com.skillsync.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateMessageRequest {
+    private String content;
+}
